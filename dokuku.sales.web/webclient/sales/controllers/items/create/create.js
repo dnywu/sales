695c9179bbb293cce7ddfdc96eaafbdc14707a52
finsa <finsa@inforsys.co.id>
2011-12-01 11:57:30 +0700

    css item create

--- dokuku.sales.web/webclient/sales/controllers/items/create/create.js
+++ dokuku.sales.web/webclient/sales/controllers/items/create/create.js
@@ -1,6 +1,7 @@
 steal('jquery/controller',
       'jquery/view/ejs',
-      'jquery/controller/view')
+      'jquery/controller/view',
+      './ItemsCreate.css')
 .then('./views/init.ejs', function ($) {
           $.Controller('Sales.Items.Create',
 {
