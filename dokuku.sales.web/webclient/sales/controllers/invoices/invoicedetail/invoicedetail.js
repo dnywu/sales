e218a1350a67c18cfdb0436e01208d635e4bcd45
IsepMulyadi <isep@inforsys.co.id>
2011-12-05 16:27:02 +0700

    force cancel invoice

--- dokuku.sales.web/webclient/sales/controllers/invoices/invoicedetail/invoicedetail.js
+++ dokuku.sales.web/webclient/sales/controllers/invoices/invoicedetail/invoicedetail.js
@@ -110,6 +110,9 @@ steal('jquery/controller', 'jquery/view/ejs',
         if (IsStatus == "Batal") {
             $("#menuItemRightBatal").remove();
         }
+        if(IsStatus != "Belum Bayar" && IsStatus == "Batal" && IsStatus != "Draft") {
+            $("#menuItemRightBatalPaksa").remove();
+        }
     },
     GetDetailCustomer: function (invoice) {
         $.ajax({
