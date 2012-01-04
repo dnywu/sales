02254a945aff8cb0b5a5416b272644d5afe307f7
Oetawan Chandra <chandra_jkt@yahoo.com>
2012-01-04 15:57:56 +0700

    dokuku sales organization project first commit

---
+++ dokuku.sales.fixture/When_create_organization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Machine.Specifications;
+using dokuku.sales.organization;
+namespace dokuku.sales.fixture
+{
+    [Subject("Creating organization")]
+    public class When_create_organization
+    {
+        private static IOrganizationRepository orgRepo;
+        private static Organization org;
+
+        Establish context = () =>
+            {
+                orgRepo = new OrganizationRepository(); 
+            };
+
+        Because of = () =>
+            {
+                org = orgRepo.Create(new Organization("oetawan@inforsys.co.id", "Inforsys Indonesia, PT", "IDR", 1));
+            };
+
+        It should_create_organization = () =>
+            {
+                org.ShouldNotBeNull();
+            };
+
+        Cleanup cleanup = () =>
+            {
+                orgRepo.Delete(org);
+            };
+    }
+}
---
+++ dokuku.sales.organization/Organization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace dokuku.sales.organization
+{
+    public class Organization
+    {
+        const string ORGANIZATION_TYPE = "organization";
+
+        public string _id { get; set; }
+        public string _rev { get; set; }
+        public string Name { get; set; }
+        public string Cucrrency { get; set; }
+        public int FiscalYearPeriod { get; set; }
+        public string Type { get; private set; }
+
+        public Organization(string id, string name, string ccy, int fiscalYearPeriod)
+        {
+            _id = id;
+            Name = name;
+            Cucrrency = ccy;
+            FiscalYearPeriod = fiscalYearPeriod;
+            Type = ORGANIZATION_TYPE;
+        }
+    }
+}
