eeb2083763d9db4f4ff6caaa1d203ab2daf5feba
Oetawan Chandra <chandra_jkt@yahoo.com>
2012-01-05 15:09:36 +0700

    dokuku sales item first commit

---
+++ dokuku.sales.item/IItemRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace dokuku.sales.item
+{
+    public interface IItemRepository
+    {
+        void Save(Item item);
+        Item Get(Guid id);
+        void Delete(Guid id);
+        IEnumerable<Item> AllItems();
+    }
+}
---
+++ dokuku.sales.item/ItemRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LoveSeat;
+using dokuku.sales.config;
+using System.Configuration;
+namespace dokuku.sales.item
+{
+    public class ItemRepository : IItemRepository
+    {
+        CouchClient couchClient;
+        CouchDatabase db;
+
+        public ItemRepository()
+        {
+            CouchDBConfig cfg = (CouchDBConfig)ConfigurationManager.GetSection("CouchDBConfig");
+            if (cfg == null)
+                throw new ApplicationException("CouchDBConfig tidak di temukan dalam app config"); 
+            couchClient = new CouchClient(cfg.Server, cfg.Port, cfg.Username, cfg.Password, false, AuthenticationType.Basic);
+        }
+
+        public void Save(Item item)
+        {
+            Document<Item> doc = new Document<Item>(item);
+            DB.SaveDocument(doc);
+        }
+
+        public Item Get(Guid id)
+        {
+            return DB.GetDocument<Item>(id);
+        }
+
+        public void Delete(Guid id)
+        {
+            Item cs = DB.GetDocument<Item>(id);
+            if (cs == null)
+                return;
+            DB.DeleteDocument(cs._id.ToString(), cs._rev);
+        }
+
+        public IEnumerable<Item> AllItems()
+        {
+            return DB.View<Item>("all_items", "view_items").Items;
+        }
+
+        private CouchDatabase DB
+        {
+            get
+            {
+                if (db == null)
+                    db = couchClient.GetDatabase("dokuku");
+                return db;
+            }
+        }
+    }
+}
