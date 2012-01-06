a46598377a950e353d50301247d137262e9d1fa9
finsa <finsa@inforsys.co.id>
2012-01-06 10:35:31 +0700

    ok

--- dokuku.sales.web/webclient/sales/controllers/items/create/create.js
+++ dokuku.sales.web/webclient/sales/controllers/items/create/create.js
@@ -24,11 +24,42 @@ steal('jquery/controller',
         $("#dialogContent").append("//sales/controllers/items/create/views/createTaxDialog.ejs", {});
     },
     "createItemsForm submit": function (el, ev) {
-        this.validationCheck();
-
+        var form = $("#createItemsForm");
+        var err = $("#error");
+        var defaults = {
+            name: $("#itemName").val(),
+            description: $("#description").val(),
+            price: $("#itemPrice").val(),
+            tax: $("#tax").val()
+        };
+        err.empty();
+        if (defaults.name !== "" && defaults.price != 0)
+            form.submit();
+        if (defaults.name == "")
+            $('<li>', { 'class': 'name', text: "Nama Barang harus di isi" }).appendTo(err.show());
+        if (defaults.price == "")
+            $('<li>', { 'class': 'price', text: "Harga harus di diisi" }).appendTo(err.show());
+        ev.preventDefault();
+        return;
     },
-    validationCheck: function () {
-        
+    "createTaxForm submit": function (el, ev) {
+        var form = $("#createTaxForm");
+        var err = $("#error");
+        var defaults = {
+            name: $("#taxName").val(),
+            percent: $("#percentTax").val()
+        };
+        err.empty();
+        if (defaults.name !== "" && defaults.percent != 0)
+            form.submit();
+        if (defaults.name == "")
+            $('<li>', { 'class': 'name', text: "Nama Pajak harus di isi" }).appendTo(err.show());
+        if (defaults.price == "")
+            $('<li>', { 'class': 'percenttax', text: "Persentase Pajak harus di diisi" }).appendTo(err.show());
+        if (defaults.price <= 0)
+            $('<li>', { 'class': 'percenttax', text: "Persentase Pajak harus lebih besar dari nol" }).appendTo(err.show());
+        ev.preventDefault();
+        return;
     }
 })
 });
