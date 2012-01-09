c0cd01b88be51a0eea1a924b7f4a92461a54df36
dnywu <denny@inforsys.co.id>
2012-01-09 16:09:45 +0700

    create home page

--- dokuku.sales.web/webclient/sales/controllers/home/home.js
+++ dokuku.sales.web/webclient/sales/controllers/home/home.js
@@ -1,11 +1,24 @@
-steal( 'jquery/controller','jquery/view/ejs',
-        'jquery/controller/view','./Home.css'
+steal('jquery/controller', 'jquery/view/ejs',
+        'jquery/controller/view',
+        './Home.css',
+        'sales/controllers/items/create',
+        'sales/controllers/setuporganization'
      )
-	.then( './views/home.ejs', function($){
-    $.Controller('sales.controllers.home',
+	.then('./views/home.ejs', function ($) {
+	    $.Controller('sales.controllers.home',
     {
-	    init : function(){
-	        this.element.html(this.view("//sales/controllers/home/views/home.ejs", Sales.Models.Companyprofile.findOne({ id: '1' })));
-	    }
+        init: function () {
+            this.element.html(this.view("//sales/controllers/home/views/home.ejs", Sales.Models.Companyprofile.findOne({ id: '1' })));
+        },
+        '#CustomerLink click': function () {
+            $("#body").empty();
+            $("#body").removeClass();
+            $("#body").sales_items_create().GetView();
+        },
+        '#InvoiceLink click': function () {
+            $("#body").empty();
+            $("#body").removeClass();
+            alert('test');
+        }
     })
-});
+	});
