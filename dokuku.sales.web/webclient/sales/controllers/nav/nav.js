2670e3ec4804bbb685899f40e6a9cddc6c9f603e
dnywu <denny@inforsys.co.id>
2012-01-10 14:33:31 +0700

    add Homepage And Sub Menu Navigation

--- dokuku.sales.web/webclient/sales/controllers/nav/nav.js
+++ dokuku.sales.web/webclient/sales/controllers/nav/nav.js
@@ -3,13 +3,19 @@ steal('jquery/controller', 'jquery/view/ejs',
         './Nav.css',
         './NavSubMenu.css',
         'sales/controllers/items/create',
-        'sales/controllers/customers'
+        'sales/controllers/customers',
+        'sales/controllers/home'
      )
 	.then('./views/nav.ejs', function ($) {
 	    $.Controller('sales.controllers.nav',
         {
             init: function () {
                 this.element.html(this.view("//sales/controllers/nav/views/nav.ejs", Sales.Models.Companyprofile.findOne({ id: '1' })));
+            },
+            '#HomeLink click': function () {
+                this.ClearContain();
+                $("#subtabs").empty();
+                $("#body").sales_home('load');
             },
             '#CustomerLink click': function () {
                 this.ClearContain();
@@ -21,13 +27,24 @@ steal('jquery/controller', 'jquery/view/ejs',
                 this.InvoiceSubMenu();
                 $("#body").sales_items_create('load');
             },
+            '#customers click': function (el) {
+                this.ClearContain();
+                this.SetBoldActivePage(el);
+                $("#body").sales_items_create('load');
+            },
+            '#emailhistory click': function (el) {
+                this.ClearContain();
+                this.SetBoldActivePage(el);
+                $("#body").sales_items_create('load');
+            },
             CustomerSubMenu: function () {
                 var submenu = $('#subtabs');
                 var 
                     container = $('<div>', { 'class': 'container' }),
                     ul = $('<ul>', { 'class': 'ulsubtabs' }),
-                    customers = $('<li>', { 'class': 'lisubtabs', id: 'customers', text: 'Customers' }),
+                    customers = $('<li>', { 'class': 'bold lisubtabs', id: 'customers', text: 'Customers' }),
                     emailhistory = $('<li>', { 'class': 'lisubtabs', id: 'emailhistory', text: 'Email History' });
+                $("#subtabs").empty();
                 container.appendTo(submenu);
                 ul.appendTo(container);
                 customers.appendTo(ul);
@@ -38,12 +55,12 @@ steal('jquery/controller', 'jquery/view/ejs',
                 var 
                     container = $('<div>', { 'class': 'container' }),
                     ul = $('<ul>', { 'class': 'ulsubtabs' }),
-                    invoices = $('<li>', { 'class': 'lisubtabs', id: 'invoices', text: 'Invoices' }),
+                    invoices = $('<li>', { 'class': 'lisubtabs bold', id: 'invoices', text: 'Invoices' }),
                     recurringinvoices = $('<li>', { 'class': 'lisubtabs', id: 'recurringinvoices', text: 'Recurring Invoices' }),
                     creditnotes = $('<li>', { 'class': 'lisubtabs', id: 'creditnotes', text: 'Credit Notes' }),
                     paymentreceived = $('<li>', { 'class': 'lisubtabs', id: 'paymentreceived', text: 'Payment Received' });
                 items = $('<li>', { 'class': 'lisubtabs', id: 'items', text: 'Items' });
-
+                $("#subtabs").empty();
                 container.appendTo(submenu);
                 ul.appendTo(container);
                 invoices.appendTo(ul);
@@ -54,7 +71,10 @@ steal('jquery/controller', 'jquery/view/ejs',
             },
             ClearContain: function () {
                 $("#body").empty();
-                $("#subtabs").empty();
+            },
+            SetBoldActivePage: function (el) {
+                $("#subtabs li").removeClass('bold');
+                el.addClass('bold');
             }
         })
 	});
