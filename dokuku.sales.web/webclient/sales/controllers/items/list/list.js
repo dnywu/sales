30d2eba0cfb34afed8afbb7a1414f862955e3a8b
finsa <finsa@inforsys.co.id>
2012-01-12 14:06:12 +0700

    ok

--- dokuku.sales.web/webclient/sales/controllers/items/list/list.js
+++ dokuku.sales.web/webclient/sales/controllers/items/list/list.js
@@ -1,9 +1,12 @@
+var start = 0, limit = 20, totalData = 0, totalPage = 0, currentPage = 1;
+
 steal('jquery/controller',
       'jquery/view/ejs',
       'jquery/controller/view',
+      'sales/models',
       './ItemList.css',
       'sales/controllers/items/create')
-	.then('./views/ItemList.ejs', function ($) {
+	.then('./views/ItemList.ejs', './views/popupEventDialog.ejs', function ($) {
 
 	    $.Controller('sales.Controllers.items.list',
 
@@ -12,12 +15,16 @@ steal('jquery/controller',
         },
         {
             init: function () {
-                this.element.html(this.view("//sales/controllers/items/list/views/ItemList.ejs"));
+                $("#body").html(this.view("//sales/controllers/items/list/views/ItemList.ejs"));
                 this.RequestAllItems();
             },
             load: function () {
-                this.element.html(this.view("//sales/controllers/items/list/views/ItemList.ejs"));
+                $("#body").html(this.view("//sales/controllers/items/list/views/ItemList.ejs"));
                 this.RequestAllItems();
+            },
+            initpagination: function () {
+                totalPage = Math.round(totalData / limit);
+                 
             },
             RequestAllItems: function () {
                 $.ajax({
@@ -32,28 +39,58 @@ steal('jquery/controller',
                     $('#body').sales_items_create();
                 }
                 else {
+                    totalData = data.length;
                     $("table.ItemList tbody").empty();
                     $.each(data, function (item) {
                         $("table.ItemList tbody").append('<tr id="itemContent' + item + '" tabindex="' + item + '">' +
-                        '<td class="itemList"><input type="checkbox" id="checkBoxItem' + item + '" value="" /></td>' +
-                        '<td class="itemList"><div class="settingButton" id="settingButton' + item + '"><img class="" src="/sales/controllers/items/list/images/setting.png"/></div></td>' +
+                        '<td class="itemList"><input type="checkbox" class="checkBoxItem" id="checkBoxItem' + item + '" value="" /></td>' +
+                        '<td class="itemList" id="settingPanel' + item + '"></td>' +
                         '<td class="itemList itemName">' + data[item].Name + '</td>' +
                         '<td class="itemList itemPrice">' + data[item].Rate + '</td></tr>');
+                        $("td#settingPanel" + item).append("//sales/controllers/items/list/views/popupEventDialog.ejs", { index: item });
                     });
                 }
             },
+            saveDataToModel: function (data) {
+                $.each(data, function (item) {
+                    new Sales.Items({
+                        id: item,
+                        namaBarang: data[item].Name,
+                        hargaBarang: data[item].Rate
+                    }).save();
+                });
+                $("#body").html("//sales/controllers/items/list/views/ItemList.ejs", Sales.Items.findAll());
+            },
             "table.ItemList tbody tr hover": function (el) {
                 var index = el.attr("tabindex");
-                $("#settingButton" + index).show();
             },
             "table.ItemList tbody tr mouseleave": function (el) {
                 var index = el.attr("tabindex");
-                $("#settingButton" + index).hide();
+                $("tr#itemContent" + index + " td#settingPanel" + index + " div.settingButton").hide();
+                $("tr#itemContent" + index + " td#settingPanel" + index + " div.popupEventDiv").hide();
             },
             "#gotoCreateItem click": function () {
                 $('#body').empty();
                 $('#body').sales_items_create('load');
+            },
+            ".settingButton click": function () {
+                var index = $("table.ItemList tbody tr").attr("tabindex");
+                $("tr#itemContent" + index + " td#settingPanel" + index + " div.popupEventDiv").show();
+            },
+            "#checkBoxAllList change": function () {
+                if ($("#checkBoxAllList").attr('checked')) {
+                    $(".checkBoxItem").attr('checked', 'checked');
+                } else {
+                    $(".checkBoxItem").removeAttr('checked');
+                }
+            },
+            "#btnEdit click": function () {
+                alert("test button edit");
+            },
+            "#btnDelete click": function () {
+                alert("test button hapus");
             }
+
         })
 
 	});
