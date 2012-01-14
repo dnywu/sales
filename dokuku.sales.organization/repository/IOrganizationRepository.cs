56483035cbbb49f9bd8d9b31074064056a787833
mart12000 <mart_12000@yahoo.com>
2012-01-14 08:53:07 +0700

    update mongodb

---
+++ dokuku.sales.organization/repository/IOrganizationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dokuku.sales.organization.model;
+namespace dokuku.sales.organization.repository
+{
+    public interface IOrganizationRepository
+    {
+        void Save(Organization org);
+        void Delete(Guid id);
+        Organization Get(Guid id);
+        Organization FindByOwnerId(string email);
+    }
+}
