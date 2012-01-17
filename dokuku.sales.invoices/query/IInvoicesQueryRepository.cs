6df94432ca780d5784ccf1a315f6ed3bda2f27d2
randiagoesta <darknesside@aol.com>
2012-01-17 14:58:27 -0800

    list invoice

--- dokuku.sales.invoices/query/IInvoicesQueryRepository.cs
+++ dokuku.sales.invoices/query/IInvoicesQueryRepository.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using dokuku.sales.invoices.model;
 
 namespace dokuku.sales.invoices.query
 {
     public interface IInvoicesQueryRepository
     {
+        IEnumerable<Invoices> AllInvoices();
     }
 }
