28c9db4c55f42a0ab2d55009eb8cd35689330d4b
mart12000 <mart_12000@yahoo.com>
2012-01-19 11:03:20 +0700

    update

---
+++ dokuku.sales.customer/Service/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dokuku.sales.customer.model;
+using dokuku.sales.customer.repository;
+using NServiceBus;
+
+namespace dokuku.sales.customer.Service
+{
+    public class CustomerService : ICustomerService
+    {
+        ICustomerRepository repo;
+        IBus bus;
+        public CustomerService(ICustomerRepository repo, IBus bus)
+        {
+            this.repo = repo;
+            this.bus = bus;
+        }
+        public void Save(Customer cs)
+        {
+            repo.Save(cs);
+            CreateIndex(cs);
+            bus.Publish<>();
+        }
+
+        private CustomerReports CreateIndex(Customer customer)
+        {
+            return new CustomerReports(customer);
+        }
+
+        public void UpdateCustomer(Customer cust)
+        {
+            repo.UpdateCustomer(cust);
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(Guid id, string ownerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ dokuku.sales.customer/Service/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using dokuku.sales.customer.model;
+
+namespace dokuku.sales.customer.Service
+{
+    public interface ICustomerService
+    {
+        void Save(Customer cs);
+        void UpdateCustomer(Customer cust);
+        void Delete(Guid id);
+        Customer Get(Guid id, string ownerId);
+    }
+}
