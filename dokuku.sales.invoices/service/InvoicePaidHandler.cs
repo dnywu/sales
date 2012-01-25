de3ef3e74407bdf27b972addab2dd8a5689ed38d
Oetawan Chandra <chandra_jkt@yahoo.com>
2012-01-25 19:21:30 +0700

    - sales report distributor - refactor sales report - payment distributor - refactor payment module

---
+++ dokuku.sales.invoices/service/InvoicePaidHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NServiceBus;
+using dokuku.sales.config;
+using dokuku.sales.invoices.model;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using dokuku.sales.invoices.query;
+using dokuku.sales.invoices.command;
+using dokuku.sales.payment.events;
+namespace dokuku.sales.invoices.service
+{
+    public class InvoicePaidHandler : IHandleMessages<InvoicePaid>
+    {
+        IInvoiceService InvoiceService { get; set; }
+        public void Handle(InvoicePaid message)
+        {
+            //InvoiceService.InvoiceFullyPaid(message.InvoiceId, message.ownerid);
+        }
+    }
+}
