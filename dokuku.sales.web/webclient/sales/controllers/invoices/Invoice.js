2d0d0cd1a3e35f7103fad033a12551f7a1025c57
finsa <finsa@inforsys.co.id>
2012-01-26 10:32:19 +0700

    edit link request invoice

--- dokuku.sales.web/webclient/sales/controllers/invoices/Invoice.js
+++ dokuku.sales.web/webclient/sales/controllers/invoices/Invoice.js
@@ -197,7 +197,7 @@
         var key = $('#SearchInvoice').val();
         $.ajax({
             type: 'GET',
-            url: '/SearchInvoice/key/' + key,
+            url: '/SearchInvoice/' + key,
             dataType: 'json',
             async: false,
             success: function (data) {
@@ -217,7 +217,7 @@
         var result;
         $.ajax({
             type: 'DELETE',
-            url: '/deleteInvoice/invoiceNo/' + invoiceNo,
+            url: '/deleteInvoice/' + invoiceNo,
             dataType: 'json',
             async: false,
             success: function (data) {
@@ -243,7 +243,7 @@
         var listCustomer = null;
         $.ajax({
             type: 'GET',
-            url: '/SearchCustomer/key/' + key,
+            url: '/SearchCustomer/' + key,
             dataType: 'json',
             failure: $('#DivSearchCustomer').hide(),
             async: false,
