329ed973f34d74b39aee44f431007d97646ae08c
Denny Wu <denny@inforsys.co.id>
2012-02-03 19:12:06 +0700

    resize, compress upload logo organization

--- dokuku.sales.web/modules/OrganizationModule.cs
+++ dokuku.sales.web/modules/OrganizationModule.cs
@@ -9,6 +9,8 @@ using Nancy;
 using Nancy.Security;
 using Newtonsoft.Json;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 namespace dokuku.sales.web.modules
 {
     public class OrganizationModule : Nancy.NancyModule
@@ -34,6 +36,13 @@ namespace dokuku.sales.web.modules
                         Currency = curr,
                         FiscalYearPeriod = starts
                     });
+                    Image img = Image.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Content\images\default-logo-organization.png"));
+                    MemoryStream imgStream = new MemoryStream();
+                    img.Save(imgStream, ImageFormat.Png);
+                    byte[] logoData = readImageAndCompress(imgStream);
+
+                    LogoOrganization logoOrganization = new LogoOrganization { _id = this.CurrentAccount().OwnerId, ImageData = logoData, OwnerId = this.CurrentAccount().OwnerId };
+                    this.LogoOrganizationCommand().Save(logoOrganization);
                 }
                 catch (Exception ex)
                 {
@@ -61,12 +70,8 @@ namespace dokuku.sales.web.modules
             Post["/uploadlogoorg"] = p =>
             {
                 Stream stream = this.Request.Files.FirstOrDefault().Value;
-                byte[] logoData = ReadFile(stream);
-                long size = logoData.LongLength / 1024;
-                if (size > 1024)
-                {
-                    return Response.AsRedirect("/?error=true");
-                }
+                byte[] logoData = readImageAndCompress(stream);
+                
                 LogoOrganization logoOrganization = new LogoOrganization { _id = this.CurrentAccount().OwnerId, ImageData = logoData, OwnerId = this.CurrentAccount().OwnerId };
                 this.LogoOrganizationCommand().Save(logoOrganization);
                 return Response.AsRedirect("/uploadlogo");
@@ -77,31 +82,89 @@ namespace dokuku.sales.web.modules
                 LogoOrganization logo = this.LogoOrganizationQuery().GetLogo(this.CurrentAccount().OwnerId);
                 if (logo == null)
                     return null;
-                MemoryStream stream = new MemoryStream(logo.ImageData);
+                MemoryStream stream = new MemoryStream(Zip7.Decompress(logo.ImageData));
                 return Response.FromStream(stream, "image/png");
             };
         }
 
-        byte[] ReadFile(Stream stream)
+        byte[] readImageAndCompress(Stream stream)
+        {
+            var image = Image.FromStream(stream);
+            image = resizeImage(image, new Size(140, 60));
+            MemoryStream imgStream = new MemoryStream();
+            image.Save(imgStream, ImageFormat.Png);
+            var comppressedImg = Zip7.Compress(imgStream.ToArray());
+            return comppressedImg;
+        }
+
+        private static byte[] readStream(Stream stream)
         {
             byte[] buffer;
-            //FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
             try
             {
-                int length = (int)stream.Length;  // get file length
-                buffer = new byte[length];            // create buffer
-                int count;                            // actual number of bytes read
-                int sum = 0;                          // total number of bytes read
+                int length = (int)stream.Length; 
+                buffer = new byte[length];
+                int count;
+                int sum = 0;
 
-                // read until Read method returns 0 (end of the stream has been reached)
                 while ((count = stream.Read(buffer, sum, length - sum)) > 0)
-                    sum += count;  // sum is a buffer offset for next reading
+                {
+                    sum += count;
+                }
             }
             finally
             {
                 stream.Close();
             }
             return buffer;
+        }
+        static byte[] cropImageFile(byte[] imageFile, int targetW, int targetH, int targetX, int targetY)
+        {
+            Image imgPhoto = Image.FromStream(new MemoryStream(imageFile));
+            Bitmap bmPhoto = new Bitmap(targetW, targetH, PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(72, 72);
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.SmoothingMode = SmoothingMode.AntiAlias;
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            grPhoto.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            grPhoto.DrawImage(imgPhoto, new Rectangle(0, 0, targetW, targetH), targetX, targetY, targetW, targetH, GraphicsUnit.Pixel);
+
+            MemoryStream mm = new MemoryStream();
+            bmPhoto.Save(mm, System.Drawing.Imaging.ImageFormat.Png);
+            imgPhoto.Dispose();
+            bmPhoto.Dispose();
+            grPhoto.Dispose();
+
+            return mm.GetBuffer();
+        }
+        static Image resizeImage(Image imgToResize, Size size)
+        {
+            int sourceWidth = imgToResize.Width;
+            int sourceHeight = imgToResize.Height;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)size.Width / (float)sourceWidth);
+            nPercentH = ((float)size.Height / (float)sourceHeight);
+
+            if (nPercentH < nPercentW)
+                nPercent = nPercentH;
+            else
+                nPercent = nPercentW;
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap b = new Bitmap(destWidth, destHeight);
+            Graphics g = Graphics.FromImage((Image)b);
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+            g.Dispose();
+
+            return (Image)b;
         }
     }
 }
